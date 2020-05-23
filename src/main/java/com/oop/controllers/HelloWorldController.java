52c71acb4058ddc102b067807690e1f4310ce500
Orestis Panagopoulos <orestispanago@gmail.com>
2020-05-23 23:54:27 +0300

    Moved measurements controllers to separate class

--- src/main/java/com/oop/controllers/HelloWorldController.java
+++ src/main/java/com/oop/controllers/HelloWorldController.java
@@ -1,22 +1,14 @@
 package com.oop.controllers;
 
 import Test.Greeting;
-import com.oop.dao.CoMeasurementDao;
-import com.oop.dao.ISubscriptionDao;
-import com.oop.dao.PmMeasurementDao;
 import com.oop.dao.UserDao;
 import com.oop.entities.AppUser;
-import com.oop.entities.CoMeasurement;
-import com.oop.entities.PmMeasurement;
-import com.oop.entities.Subscription;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,36 +49,5 @@ public class HelloWorldController {
     public Optional<AppUser> read(@PathVariable int id) {
         return userService.findById(id);
     }
-
-    @Autowired
-    PmMeasurementDao pmService;
-
-    @RequestMapping(value="/pm", method=RequestMethod.POST, produces = "application/json")
-    public String pmMeasurement(@RequestBody PmMeasurement pm) {
-        pmService.saveAndFlush(pm);
-        return "pm measurement saved!";
-    }
-    
-    @Autowired
-    CoMeasurementDao coService;
-
-    @RequestMapping(value="/co", method=RequestMethod.POST, produces = "application/json")
-    public String coMeasurement(@RequestBody CoMeasurement co) {
-        coService.saveAndFlush(co);
-        return "co measurement saved!";
-    }
-    
-    
-    @Autowired
-    ISubscriptionDao subService;
-
-    @RequestMapping(value="/subscription", method=RequestMethod.POST, produces = "application/json")
-    public String coMeasurement(@RequestBody Subscription sub) {
-        System.out.println(sub);
-        subService.save(sub);
-        System.out.println(sub);
-        return "subscription saved!";
-    }
-    
     
 }
---
+++ src/main/java/com/oop/controllers/MeasurementsController.java
@@ -0,0 +1,49 @@
+package com.oop.controllers;
+
+import com.oop.dao.CoMeasurementDao;
+import com.oop.dao.ISubscriptionDao;
+import com.oop.dao.PmMeasurementDao;
+import com.oop.entities.CoMeasurement;
+import com.oop.entities.PmMeasurement;
+import com.oop.entities.Subscription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeasurementsController {
+
+    @Autowired
+    PmMeasurementDao pmService;
+
+    @RequestMapping(value="/pm", method=RequestMethod.POST, produces = "application/json")
+    public String pmMeasurement(@RequestBody PmMeasurement pm) {
+        pmService.saveAndFlush(pm);
+        return "pm measurement saved!";
+    }
+    
+    @Autowired
+    CoMeasurementDao coService;
+
+    @RequestMapping(value="/co", method=RequestMethod.POST, produces = "application/json")
+    public String coMeasurement(@RequestBody CoMeasurement co) {
+        coService.saveAndFlush(co);
+        return "co measurement saved!";
+    }
+    
+    
+    @Autowired
+    ISubscriptionDao subService;
+
+    @RequestMapping(value="/subscription", method=RequestMethod.POST, produces = "application/json")
+    public String coMeasurement(@RequestBody Subscription sub) {
+        System.out.println(sub);
+        subService.save(sub);
+        System.out.println(sub);
+        return "subscription saved!";
+    }
+    
+    
+}
