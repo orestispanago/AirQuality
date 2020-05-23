80b958f37234323ea10347d610e598a69dc59531
Orestis Panagopoulos <orestispanago@gmail.com>
2020-05-23 17:05:40 +0300

    Added /, /products and /map controllers (permitAll)

--- src/main/java/com/oop/controllers/HelloWorldController.java
+++ src/main/java/com/oop/controllers/HelloWorldController.java
@@ -6,6 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@RequestMapping({ "/" })
+	public String landingPage() {
+		return "This is the landing page";
+	}
+	@RequestMapping({ "/products" })
+	public String login() {
+		return "Products, shop or whateva";
+	}
+	@RequestMapping({ "/map" })
+	public String map() {
+		return "Here is a map for everyone";
+	}
 	@RequestMapping({ "/hello" })
 	public String firstPage() {
 		return "Hello World";
