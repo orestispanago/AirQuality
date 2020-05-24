065a18dadd4a20bd0b1f01e1be245f269b8989a9
orestispanago <orestispanago@gmail.com>
2020-05-24 19:01:09 +0300

    Added Cart+Sensor CRUD funcionality in UserController

--- src/main/java/com/oop/controllers/UserController.java
+++ src/main/java/com/oop/controllers/UserController.java
@@ -1,20 +1,48 @@
 package com.oop.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author orestis
  */
-
 @RestController
+@RequestMapping("/user/{id}")
 public class UserController {
-    @RequestMapping({"/user/sensors"})
-    public String sensors(){
-        return "A list of a user's sensors";
-    }
+
     // /user/sensor/data
     // /user/sensor/register
     // /user/sensor/delete
+    @RequestMapping(value = "/sensors", method = RequestMethod.GET, produces = "application/json")
+    public String sensors() {
+        return "List of all user's sensors";
+    }
+
+    @RequestMapping(value = "/sensor/{sensorid}/data", method = RequestMethod.POST, produces = "application/json")
+    public String registerSensor() {
+        return "Registered sensor";
+    }
+
+    @RequestMapping(value = "/sensor/{sensorid}", method = RequestMethod.DELETE, produces = "application/json")
+    public String deleteSensor() {
+        return "Deleted sensor";
+    }
+
+    @RequestMapping(value = "/cart", method = RequestMethod.GET, produces = "application/json")
+    public String viewCart() {
+        return "List of products in user's cart";
+    }
+
+    @RequestMapping(value = "/cart", method = RequestMethod.POST, produces = "application/json")
+    public String updateCart() {
+        return "Updated Cart";
+    }
+
+    @RequestMapping(value = "/cart", method = RequestMethod.DELETE, produces = "application/json")
+    public String emptyCart() {
+        return "Emptied Cart";
+    }
+
 }
