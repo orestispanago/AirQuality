7ade1a4fb1b4649effd0708482c3db712551bec5
orestispanago <orestispanago@gmail.com>
2020-05-25 14:11:18 +0300

    Fixed SensorsControllers

--- src/main/java/com/oop/controllers/SensorsController.java
+++ src/main/java/com/oop/controllers/SensorsController.java
@@ -1,9 +1,35 @@
 package com.oop.controllers;
 
+import com.oop.dao.ISensorLocationDao;
+import com.oop.dao.ISoldSensorDao;
+import com.oop.entities.SensorLocation;
+import com.oop.entities.SoldSensor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-public class SensorsController {
 @RestController
 @RequestMapping("/sensor")
+public class SensorsController {
+
+    @Autowired
+    ISensorLocationDao slService;
+
+    @RequestMapping(value = "/sensorlocation", method = RequestMethod.POST, produces = "application/json")
+    public String sensorLocation(@RequestBody SensorLocation sl) {
+        slService.save(sl);
+        return sl + " saved!";
+    }
+
+    @Autowired
+    ISoldSensorDao ssService;
+
+    @RequestMapping(value = "/soldsensoruser", method = RequestMethod.POST, produces = "application/json")
+    public String soldSensorUser(@RequestBody SoldSensor ss) {
+        ssService.save(ss);
+        return ss + " saved!";
+    }
+
 }
