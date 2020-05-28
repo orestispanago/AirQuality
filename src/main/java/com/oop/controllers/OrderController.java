0e22fbf1a6bae0aefafaf007957d00e227a0d3db
orestispanago <orestispanago@gmail.com>
2020-05-28 20:57:49 +0300

    Wrote orders algorithm in OrderController

--- src/main/java/com/oop/controllers/OrderController.java
+++ src/main/java/com/oop/controllers/OrderController.java
@@ -22,17 +22,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
-    
+
     @Autowired
     IOrderService orderservice;
-    
+
     @Autowired
     UserDao userService;
-    
+
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET, produces = "application/json")
-    public Order readOrderByUserId(@PathVariable long userId){
-        if(userService.existsById(userId) == false) throw new UserNotFoundException();
+    public Order readOrderByUserId(@PathVariable long userId) {
+        if (userService.existsById(userId) == false) {
+            throw new UserNotFoundException();
+        }
         return orderservice.getByUserId(userId);
     }
+
+    @RequestMapping(value = "/{userId}", method = RequestMethod.GET, produces = "application/json")
+    public List<Order> readAllOrdersByUserId(@PathVariable long userId) {
+        if (userService.existsById(userId) == false) {
+            throw new UserNotFoundException();
+        }
+        return orderservice.getAllByUserId(userId);
+    }
+
+
+    //  Create OrderRequest model that holds Cart  + Shipping Address
+    // Cart and shipping address come from front
     
+    @RequestMapping(value = "/{userId}", method = RequestMethod.POST, produces = "application/json")
+    public Order saveOrderByUserId(@RequestBody OrderRequest orderRequest, long userId) {
+        if (userService.existsById(userId) == false) {
+            throw new UserNotFoundException();
+        }
+        return orderservice.getByUserId(userId);
+    }
+    // orderService:
+    // List<CartItems> getCartItemsFromCart(Cart cart)
+    // Order()
+    
+    // Make orderItems for each cartItem
+        // Product getProductFromCartItem()
+        // void orderItem.setProduct(Product product)
+        // void orderItem.setQuantity(CartItem.getQuantity())   
+        // Order.setOrderItem(orderItem)
+    // order.setShippingAddress(orderRequest.getShippingAddress())
+    // order.calcTotalPrice()
+    
+    // orderService.save(order)
+    // sendMail(order)
+    // emptyCart(cart)
 }
---
+++ src/main/java/com/oop/models/OrderRequest.java
@@ -0,0 +1,11 @@
+package com.oop.models;
+
+import com.oop.entities.Cart;
+
+
+public class OrderRequest {
+
+    private Cart cart;
+    private String shippingAddress;
+    
+}
