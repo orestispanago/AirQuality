ebdd9263f629035b4aa59c68e4387ef6974a397c
orestispanago <orestispanago@gmail.com>
2020-06-04 15:23:30 +0300

    WARNING: changed db IP in application properties and models.DatabaseAdded
    CurrentController to get current pm values for all sensors

--- src/main/java/com/oop/controllers/CurrentController.java
+++ src/main/java/com/oop/controllers/CurrentController.java
@@ -2,6 +2,7 @@ package com.oop.controllers;
 
 import com.oop.models.CurrentPm;
 import com.oop.services.CurrentPmService;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,8 @@ public class CurrentController {
 //    ICurrentPmDao currentPmDao;
 //    
     @GetMapping(produces = "application/json")
-    public CurrentPm getCurrentPm() {
-       return CurrentPmService.getCurrent1();
+    public List<CurrentPm> getCurrentPm() {
+       return CurrentPmService.getCurrentPmforAllSensors();
 
  }
     
