710c10601fe82ee1c8daafc6cca76124a658fbd0
orestispanago <orestispanago@gmail.com>
2020-06-05 03:17:40 +0300

    Added more sensor locations in jsons.py and helper files

--- src/main/java/com/oop/controllers/CurrentController.java
+++ src/main/java/com/oop/controllers/CurrentController.java
@@ -3,6 +3,7 @@ package com.oop.controllers;
 import com.oop.models.CurrentPm;
 import com.oop.services.CurrentPmService;
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author orestis
  */
 @RestController
+@CrossOrigin(origins="http://localhost:3001")
 @RequestMapping("/current")
 public class CurrentController {
 //    @Autowired
