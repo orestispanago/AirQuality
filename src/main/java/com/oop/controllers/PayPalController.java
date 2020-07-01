a4f297ba240a4c7bb09a4acf30318c1a17b8a7cf
Orestis Tanis <tanisorestis@yahoo.com>
2020-07-01 17:00:11 +0300

    Cleaned PayPalController, IService and Service(also made Service's string instance variables final)

--- src/main/java/com/oop/controllers/PayPalController.java
+++ src/main/java/com/oop/controllers/PayPalController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.oop.controllers;
 
 import com.oop.services.IPayPalService;
@@ -15,10 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author petros_trak
- */
 @CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping(value = "/paypal")
