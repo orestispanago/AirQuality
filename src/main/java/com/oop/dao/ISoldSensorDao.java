87d28aed849456f3050db058c0ca86fce75e3ebf
Orestis Tanis <tanisorestis@yahoo.com>
2020-07-02 14:35:49 +0300

    Deleted boilerplate comments from all daos

--- src/main/java/com/oop/dao/ISoldSensorDao.java
+++ src/main/java/com/oop/dao/ISoldSensorDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.oop.dao;
 
 import com.oop.entities.SoldSensor;
@@ -10,10 +5,6 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Walter
- */
 @Repository
 public interface ISoldSensorDao extends CrudRepository<SoldSensor, Long> {
     List<SoldSensor> findAllByUserId(long userId);
