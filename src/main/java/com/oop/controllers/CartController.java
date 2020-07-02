809be7618ad3a0bf99071a22dc0dcd32de53f8b9
Orestis Tanis <tanisorestis@yahoo.com>
2020-07-02 14:50:54 +0300

    Removed unused imports from CartController and JwtAuthenticationController

--- src/main/java/com/oop/controllers/CartController.java
+++ src/main/java/com/oop/controllers/CartController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.oop.dao.IUserDao;
 import com.oop.dtos.CartDTO;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
--- src/main/java/com/oop/controllers/JwtAuthenticationController.java
+++ src/main/java/com/oop/controllers/JwtAuthenticationController.java
@@ -1,7 +1,5 @@
 package com.oop.controllers;
 
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
