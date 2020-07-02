9727d57cb382018637edde2ec5fcfded1bbc764a
Orestis Tanis <tanisorestis@yahoo.com>
2020-07-02 10:44:56 +0300

    added NO_CONTENT as responseStatus for OrderController DeleteMapping

--- src/main/java/com/oop/controllers/OrderController.java
+++ src/main/java/com/oop/controllers/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.oop.dao.IUserDao;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,6 +48,7 @@ public class OrderController {
 //    }
     
     @DeleteMapping(value = "/{orderId}")
+    @ResponseStatus(NO_CONTENT)
     public void deleteOrder(@PathVariable long orderId){
         orderService.deleteById(orderId);
     }
